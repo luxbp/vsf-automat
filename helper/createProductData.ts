3a9250ceec2c647576ef7d1ce53c81a019b83853
Brian McDonald <bmcdonald@luxbp.com>
2021-07-08 12:13:59 -0400

    feat(product-data): use final price on item

--- helper/createProductData.ts
+++ helper/createProductData.ts
@@ -2,7 +2,7 @@ export default (product, opts: Record<string, any> = {}) => {
   return {
     id: product.sku,
     variantIds: [product.sku],
-    price: parseInt((parseFloat(product.price || 0) * 100).toFixed(0), 10),
+    price: parseInt((parseFloat(product.final_price || 0) * 100).toFixed(0), 10),
     quantity: product.qty || opts.qty,
     discounted: !!opts.coupon_code,
     displayName: product.name
